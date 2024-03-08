795a89b720210f7547b4f2eb96e96541787ae7da
hyejeong <112006114+hyezg@users.noreply.github.com>
2024-03-08 16:52:40 +0900

    과제 영상본 후 수정본

--- app/src/main/java/com/example/kotlin_study/Calculate.kt
+++ app/src/main/java/com/example/kotlin_study/Calculate.kt
@@ -3,55 +3,64 @@ package com.example.kotlin_study
 
 fun main() {
     while(true) {
-        var num1: Int = readLine()!!.toInt()
-        var num2: Int = readLine()!!.toInt()
-        var op: Int = readLine()!!.toInt()
+        val addcal = AddOperation()
+        val subcal = SubstractOperation()
+        val multical = MultiplyOperation()
+        val divical = DivideOperation()
 
-        var cal = Calculator()
-
-        if(num1 == -1 || num2 ==-1){
-            println("예외")
+        println("num1을 입력하세요.")
+        val num1: Int = readLine()!!.toInt()
+        if(num1 == -1) {
+            println("-1을 입력해 중단되었습니다.")
             break
         }
 
-        var result = when(op) {
-            1 -> AddOperation().calculate(num1,num2,op)
-            2 -> SubstractOperation().calculate(num1,num2,op)
-            3 -> MultiplyOperation().calculate(num1,num2,op)
-            4 -> DivideOperation().calculate(num1,num2,op)
-            else -> throw IllegalAccessException("예외")
+        println("num2을 입력하세요.")
+        val num2: Int = readLine()!!.toInt()
+        if(num2 == -1) {
+            println("-1을 입력해 중단되었습니다.")
+            break
         }
-        println("${result}")
+
+        println("연산을 선택하세요 [+ : 1], [- : 2], [* : 3], [/ : 4] 입니다.")
+        val op: Int = readLine()!!.toInt()
+
+        when(op) {
+            1 -> addcal.calculate(num1,num2)
+            2 -> subcal.calculate(num1,num2)
+            3 -> multical.calculate(num1,num2)
+            4 -> divical.calculate(num1,num2)
+            else -> throw IllegalAccessException("1~4까지 범위안의 숫자를 입력해야합니다.")
+        }
     }
 }
 
-
 open class Calculator() {
-    open fun calculate(num1: Int, num2: Int, op: Int): Int {
-        return 0
+    open fun calculate(num1: Int, num2: Int){
+
     }
 
 }
 
 class AddOperation : Calculator() {
-    override fun calculate(num1: Int, num2: Int, op: Int): Int {
-        return num1+num2
+    override fun calculate(num1: Int, num2: Int)  {
+        println("num1+num2 = ${num1+num2}")
     }
 }
 
 class SubstractOperation : Calculator() {
-    override fun calculate(num1: Int, num2: Int, op: Int): Int {
-        return num1-num2
+    override fun calculate(num1: Int, num2: Int) {
+        println("num1-num2 = ${num1-num2}")
     }
 }
 class MultiplyOperation : Calculator() {
-    override fun calculate(num1: Int, num2: Int, op: Int): Int {
-        return num1*num2
+    override fun calculate(num1: Int, num2: Int) {
+        println("num1*num2 = ${num1*num2}")
     }
 }
 class DivideOperation : Calculator() {
-    override fun calculate(num1: Int, num2: Int, op: Int): Int {
-        return num1/num2
+    override fun calculate(num1: Int, num2: Int) {
+        println("num1/num2 = ${num1/num2}")
     }
 }
 
